034c3381fffdd62d8eacb40b156e2584ecc19051
hdqeing <hdqeing@gmail.com>
2025-04-16 20:00:10 +0200

    Remove seller ui

--- api/src/main/java/com/palette/api/controller/AuthController.java
+++ api/src/main/java/com/palette/api/controller/AuthController.java
@@ -1,25 +1,36 @@
-//package com.palette.api.controller;
-//
-//import org.apache.catalina.User;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//@RequestMapping("/auth")
-//@RestController
-//public class AuthController {
-//    @PostMapping("/signup")
-//    public ResponseEntity<User> register(){
-//        User registeredUser = authenticationService.signup();
-//        return ResponseEntity.ok(registeredUser);
-//    }
-//
-//    @PostMapping("/login")
-//    public ResponseEntity<LoginResponse> authenticate(){
-//        User authenticatedUser = authenticationService.authenticate();
-//        String jwtToken = jwtService.generateToken(authenticatedUser);
-//        LoginResponse loginResponse = new LoginResponse().serToken(jwtToken).setExpiresIn();
-//        return ResponseEntity.ok(LoginResponse);
-//    }
-//}
+package com.palette.api.controller;
+
+import com.palette.api.service.AuthenticationService;
+import com.palette.api.service.JwtService;
+import org.apache.catalina.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/auth")
+@RestController
+public class AuthController {
+
+    private final JwtService jwtService;
+    private final AuthenticationService authenticationService;
+
+    public AuthController(JwtService jwtService, AuthenticationService authenticationService) {
+        this.jwtService = jwtService;
+        this.authenticationService = authenticationService;
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<User> register(){
+        User registeredUser = authenticationService.signup();
+        return ResponseEntity.ok(registeredUser);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> authenticate(){
+        User authenticatedUser = authenticationService.authenticate();
+        String jwtToken = jwtService.generateToken(authenticatedUser);
+        LoginResponse loginResponse = new LoginResponse().serToken(jwtToken).setExpiresIn();
+        return ResponseEntity.ok(LoginResponse);
+    }
+}
--- api/src/main/java/com/palette/api/service/SellerDetailsService.java
+++ api/src/main/java/com/palette/api/service/SellerDetailsService.java
@@ -1,32 +1,29 @@
-//package com.palette.api.service;
-//
-//import com.palette.api.model.Seller;
-//import com.palette.api.repository.SellerRepository;
-//import org.springframework.security.authentication.AuthenticationManager;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//import org.springframework.stereotype.Service;
-//
-//@Service
-//public class SellerAuthService {
-//    private final SellerRepository sellerRepository;
-//    private final PasswordEncoder passwordEncoder;
-//    private final AuthenticationManager authenticationManager;
-//
-//    public SellerAuthService(SellerRepository sellerRepository, PasswordEncoder passwordEncoder, AuthenticationManager authenticationManager) {
-//        this.sellerRepository = sellerRepository;
-//        this.passwordEncoder = passwordEncoder;
-//        this.authenticationManager = authenticationManager;
-//    }
-//
-//    public Seller signup(){
-//        Seller seller = new Seller();
-//        seller.setEmail("myemail");
-//        return seller;
-//    }
-//
-//    public Seller authenticate(){
-//        return sellerRepository.findByEmail();
-//    }
-//
-//
-//}
+package com.palette.api.service;
+
+import com.palette.api.model.Seller;
+import com.palette.api.repository.SellerRepository;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SellerDetailsService implements UserDetailsService {
+    private final SellerRepository sellerRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+
+    public SellerDetailsService(SellerRepository sellerRepository, PasswordEncoder passwordEncoder, AuthenticationManager authenticationManager) {
+        this.sellerRepository = sellerRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.authenticationManager = authenticationManager;
+    }
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
+    }
+}
